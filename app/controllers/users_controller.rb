89052e35b97366be16dd90255ba1ca87fa7f8ecd
keiblue <f.malermo@gmail.com>
2017-03-30 17:50:33 -0300

    'holi'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   # GET /users/new
   def new
     @user = User.new
+
   end
 
   # GET /users/1/edit
@@ -69,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:Name, :Nick, :email, :address, :Password, :ZipCode, :phone)
+      params.require(:user).permit(:Name, :Nick, :email, :address, :Password, :ZipCode, :phone, :Password2)
     end
 end
