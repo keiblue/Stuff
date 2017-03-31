537c2b5a39e1de73887c2c4b863cd1e58f22d56b
kei <f.malermo@gmail.com>
2017-03-31 05:52:00 -0300

    hi

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:Name, :Nick, :email, :address, :Password, :ZipCode, :phone, :Password2)
+      params.require(:user).permit(:Name, :Nick, :email, :address, :Password, :ZipCode, :phone)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+	validates :Name , presence: true
+	validates :Nick , presence: true, uniqueness: true
+	validates :email , presence: true, uniqueness: true
+
 end
