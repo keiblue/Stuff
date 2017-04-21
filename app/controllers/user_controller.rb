a06453064465107481627f621c9dea2a0c028846
kei <f.malermo@gmail.com>
2017-04-21 01:11:13 -0300

    segunda prueba subida de imaenes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,3 @@
 class UserController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
 	after_create :save_categories
-	
+
 	has_attached_file(:picture,
       styles: {medium: "600x400", thumb: "300x200"},
       default_url: "/images/:style/missing_product.png",
@@ -8,11 +8,11 @@ class Product < ApplicationRecord
       :storage => :s3,
       :path => "/images/products/product_:product_id/:style/product.:extension"
  )
- Paperclip.interpolates :product_id  do |attachment, style|
+ 	Paperclip.interpolates :product_id  do |attachment, style|
 	 attachment.instance.id
- end
+ 	end
   validates_attachment_content_type :picture, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png)$/, :message => 'file type is not allowed (only jpeg/png/jpg images)'
-
+	validates_attachment_size :picture, :in => 0.megabytes..1.megabytes
 
  	def categories=(value)
  		@categories = value
